ffe1af46777d8be25286e5ab9851523c808e7031
zomutt <christiejcomer@gmail.com>
2025-11-17 22:39:28 -0500

    merged level 1 scenes, added my new assets and such

---
+++ Assets/Scripts/LightSensitiveEntity.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LightSensitiveEntity : MonoBehaviour
+{
+//This script is meant for entities that are sensitive to light, specific entities with different mechanics may have other scripts attached to them but this is the general one
+
+}
