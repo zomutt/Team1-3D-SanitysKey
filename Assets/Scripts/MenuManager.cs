2eaee41081418350c34930ce380814ada1e9896b
zomutt <christiejcomer@gmail.com>
2025-12-01 15:48:16 -0500

    level 2 started

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject FirstPanel;
+    public GameObject ConfirmPanel;
+
+    private void Awake()
+    {
+        FirstPanel.SetActive(true);
+        ConfirmPanel.SetActive(false);
+    }
+    void FirstQuitOnClick()
+    {
+        FirstPanel.SetActive(false);
+        ConfirmPanel.SetActive(true);
+    }
+
+    void PersistOnClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void ConfirmQuitOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void RestartGameOnClick() { SceneManager.LoadScene("Level1"); }
+}
